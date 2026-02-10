7e88b677ec36ab7cc5fac6df699ebd02ed1dcc6b
Aditya Rajendra Parakhi <80239927+adityaparakhi@users.noreply.github.com>
2026-02-11 00:21:42 +0530

    Fix formatting and comments in decimal_conversion_q2.py

--- decimal_conversion_q2.py
+++ decimal_conversion_q2.py
@@ -7,7 +7,7 @@ input1 = 0b0110
 input2 = 2
 '''
 
-number = input("Enter number in binary, octal, or hexadecimal format: ")  # This number can be in binary, octal, or hexadecimal format
+number = input("Enter number in binary, octal, or hexadecimal format: ")  # This number can be in binary, octal or hexadecimal numbers
 
 base = int(input("Enter the base (2 = binary, 8 = octal, 16 = hexadecimal): "))  # Accept base of the number 
 
@@ -16,3 +16,4 @@ decimal_value = int(number, base)  # Converting the number into decimal form
 print("=================================")
 print("Decimal form of Input Number is ", decimal_value)   # Display result
 print("=================================")
+
