e6cfdd63ef7f08d94de65280345223a36900c814
Aditya Rajendra Parakhi <80239927+adityaparakhi@users.noreply.github.com>
2026-02-22 00:53:37 +0530

    Add files via upload

---
+++ list_operation.py
@@ -0,0 +1,32 @@
+print("=====================")
+print("|| List Operations ||")
+print("=====================")
+
+rcb = ["1. Philip Salt (WK-Batter)", "2. Virat Kohli (Batter)", "3. Mayank Agarwal (Batter)", "4. Rajat Patidar (c) (Batter)", "5. Liam Livingstone (Batting All-Rounder)", "6. Jitesh Sharma (Wicket-Keeper / Batter)", "7. Romario Shepherd (All-Rounder)", "8. Krunal Pandya (Batting All-Rounder)", "9. Bhuvneshwar Kumar (Bowler)", "10. Yash Dayal (Bowler)", "11. Josh Hazlewood (Bowler)"]
+print("RCB 2025 Players Names -->\n")
+print(rcb)
+# print("\nType of variable rcb is:", type(rcb), "\nId of varible rcb is:", id(rcb))
+print("=============================================================================================================================================================================================")
+
+
+pbks = ["1. Priyansh Arya (Batter)", "2. Josh Inglis (WK-Batter)", "3. Shreyas Iyer (c) (Batter)", "4. Nehal Wadhera (Batter)", "5. Shashank Singh (Batting All-Rounder)", "6. Marcus Stoinis (Batting All-Rounder)", "7. Azmatullah Omarzai (Bowling All-Rounder)", "8. Kyle Jamieson (Bowler)", "9. Vijaykumar Vyshak (Bowler)", "10. Arshdeep Singh (Bowler)", "11. Yuzvendra Chahal (Bowler)"]
+print("\nPBKS 2025 Players Names -->")
+print(pbks)
+# print("\nType of variable pbks is:", type(pbks), "\nId of variable pbks", id(pbks))
+print("=============================================================================================================================================================================================")
+
+
+mi = ["1. Rohit Sharma (Batter)", "2. Ishan Kishan (WK-Batter)", "3. Suryakumar Yadav (Batter)", "4. Tilak Varma (Batter)", "5. Hardik Pandya (c) (All-Rounder)", "6. Tim David (Batting All-Rounder)", "7. Nehal Wadhera (Batter)", "8. Piyush Chawla (Bowler)", "9. Jasprit Bumrah (Bowler)", "10. Gerald Coetzee (Bowler)", "11. Akash Madhwal (Bowler)"]
+print("\nMI 2025 Players Names -->")
+print(mi)
+# print("\nType of variable mi is:", type(mi), "\nId of variable mi", id(mi))
+print("=============================================================================================================================================================================================")
+
+
+
+gt = ["1. Shubman Gill (c) (Batter)", "2. Wriddhiman Saha (WK-Batter)", "3. Sai Sudharsan (Batter)", "4. David Miller (Batter)", "5. Vijay Shankar (All-Rounder)", "6. Rahul Tewatia (All-Rounder)", "7. Rashid Khan (Bowler)", "8. Noor Ahmad (Bowler)", "9. Mohit Sharma (Bowler)", "10. Mohammed Shami (Bowler)", "11. Spencer Johnson (Bowler)"]
+
+print("\nGT 2025 Players Names -->")
+print(gt)
+# print("\nType of variable gt is:", type(gt), "\nId of variable gt", id(gt))
+print("=============================================================================================================================================================================================")
