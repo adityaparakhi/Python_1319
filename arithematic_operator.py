0ad2cdbd8fc9bd745f7f651cfba141638666f928
Aditya Rajendra Parakhi <80239927+adityaparakhi@users.noreply.github.com>
2026-03-09 13:02:15 +0530

    Add files via upload

---
+++ arithematic_operator.py
@@ -0,0 +1,68 @@
+'''
+Note/ Table
+    +       int + int     True + True     list1 + list2     tuple + tuple       dict + dict     
+    -
+    *
+    /
+    //
+'''
+
+'''
+int + str
+int + list
+int + tuple
+int + set
+int + dict
+
+
+str * list
+str * tuple
+str * set
+str * dict
+'''
+
+# make that home work with all data types
+
+
+# int
+print("int + int =", 10 + 5)
+print("int - int =", 10 - 5)
+print("int * int =", 10 * 5)
+print("int / int =", 10 / 5)
+print("int // int =", 10 // 5)
+
+# int with float
+print("int + float =", 10 + 2.5)
+print("int - float =", 10 - 2.5)
+print("int * float =", 10 * 2.5)
+print("int / float =", 10 / 2.5)
+print("int // float =", 10 // 2.5)
+
+# bool
+print("True + True =", True + True)
+print("True - False =", True - False)
+print("True * 5 =", True * 5)
+print("True / 2 =", True / 2)
+print("True // 2 =", True // 2)
+
+# string
+print("string + string =", "Hello" + "World")
+print("string * int =", "Hi" * 3)
+
+# list
+print("list + list =", [1,2] + [3,4])
+print("list * int =", [1,2] * 2)
+
+# tuple
+print("tuple + tuple =", (1,2) + (3,4))
+print("tuple * int =", (1,2) * 2)
+
+# set (only subtraction works)
+print("set - set =", {1,2,3} - {2})
+
+# float
+print("float + float =", 2.5 + 1.5)
+print("float - float =", 2.5 - 1.5)
+print("float * float =", 2.5 * 1.5)
+print("float / float =", 2.5 / 1.5)
+print("float // float =", 2.5 // 1.5)
